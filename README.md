45050b8f4aaf836de869013d07fd820eb4307768
Charles Read <charles@charlesread.io>
2018-07-15 09:06:30 -0400

    initial

---
+++ README.md
@@ -0,0 +1,5 @@
+This is a laughably simple fastify example, used mostly in testing my containerized computing stacks.
+
+```bash
+
+```
---
+++ index.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const address = require('ip').address()
+const fastify = require('fastify')()
+
+fastify.get('/', async function (req, reply) {
+  console.log(`endpoint / on ${address} was requested at ${new Date()}`)
+  return reply.send({
+    address: address,
+    date: new Date()
+  })
+})
+
+const start = async () => {
+  try {
+    await fastify.listen(8080, address)
+    console.log(`server listening at ${address}:${fastify.server.address().port}`)
+  } catch (err) {
+    console.error(err.message)
+    process.exit(1)
+  }
+}
+
+start()
